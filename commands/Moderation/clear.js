5bb5da7ce402ff17fc970dbea9447405088ba693
LePtitMetalleux <59097429+LePtitMetalleux@users.noreply.github.com>
2020-01-10 23:27:12 +0100

    Add files via upload

---
+++ commands/Moderation/clear.js
@@ -0,0 +1,34 @@
+const Command = require("../../base/Command.js");
+
+class clear extends Command {
+    constructor(client) {
+        super(client, {
+            name: "clear",
+            description: (language) => language.get("CLEAR_DESCRIPTION"),
+            usage: (language, prefix) => language.get("CLEAR_USAGE", prefix),
+            examples: (language, prefix) => language.get("CLEAR_EXAMPLE", prefix),
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: false,
+            permLevel: "Server Moderator",
+            botPermissions: ["MANAGE_MESSAGES"],
+            nsfw: false,
+            adminOnly: false,
+            cooldown: 1000,
+        });
+    }
+
+    async run(message) {
+        try {
+            message.channel.fetchMessages()
+                .then(function (list) {
+                    message.channel.bulkDelete(list)
+                })
+        } catch (error) {
+            console.error(error);
+            return message.channel.send(message.language.get("ERROR", error));
+        }
+    }
+}
+
+module.exports = clear;
