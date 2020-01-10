270e4d006e722ff96904562cea9fcce2e036b12e
LePtitMetalleux <59097429+LePtitMetalleux@users.noreply.github.com>
2020-01-10 23:07:33 +0100

    Add files via upload

---
+++ commands/Moderation/unmute.js
@@ -0,0 +1,53 @@
+const Command = require("../../base/Command.js");
+
+class unmute extends Command {
+    constructor(client) {
+        super(client, {
+            name: "mute",
+            description: (language) => language.get("MUTE_DESCRIPTION"),
+            usage: (language, prefix) => language.get("MUTE_USAGE", prefix),
+            examples: (language, prefix) => language.get("MUTE_EXAMPLE", prefix),
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            permLevel: "Server Moderator",
+            botPermissions: ["MANAGE_ROLES", "SEND_MESSAGES"],
+            nsfw: false,
+            adminOnly: false,
+            cooldown: 1000,
+        });
+    }
+
+    async run(message, args) {
+        try {
+            const searchArgs = args.join(" ");
+            if (!searchArgs) {
+                return message.channel.send(`<:false:470303149077299231> ${message.language.get("MUTE_ERRORARGS")}`)
+            } else {
+                let member;
+                if (message.mentions.members.size > 0) {
+                    member = message.mentions.members.first();
+                } else if (searchArgs) {
+                    member = message.bot.functions.fetchMembers(message.guild, searchArgs);
+                    if (member.size === 0) return message.channel.send(message.language.get("ERROR_NOUSER_FOUND"));
+                    else if (member.size === 1) member = member.first();
+                    else return message.channel.send(message.language.get("ERROR_MUCH_USER_FOUND"));
+                }
+                let muteRole = message.guild.roles.find(m => m.name === 'muted');
+                if (!member.roles.some(r => r.name === 'muted')) return message.channel.send(message.language.get("UNMUTE_USER_NOT_MUTED"));
+                await member.removeRole(muteRole.id)
+                    .then(r => {
+                        message.channel.send(message.language.get("UNMUTE_SUCCESS", member));
+                        member.send(message.language.get("UNMUTE_USER_SUCCESS", message))
+                    })
+                    .catch((error) => message.channel.send(`<:false:470303149077299231> ${message.author} ${message.language.get("UNMUTE_ERROR")} ${error}`));
+            }
+        }
+        catch (error) {
+            console.error(error);
+            return message.channel.send(message.language.get("ERROR", error));
+        }
+    }
+}
+
+module.exports = unmute;
