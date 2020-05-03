6684d0621fe9112b83602aa2396323ff9e84a9d5
LePtitMetalleux <iias.bv@gmail.com>
2020-05-03 18:12:16 +0200

    Add blague command

---
+++ commands/Fun/blague.js
@@ -0,0 +1,63 @@
+const Command = require("../../base/Command.js");
+const fetch = require("node-fetch");
+class Number extends Command {
+    constructor(client) {
+        super(client, {
+            name: "blague",
+            description: (language) => language.get("CAT_DESCRIPTION"),
+            usage: (language, prefix) => language.get("CAT_USAGE", prefix),
+            examples: (language, prefix) => language.get("CAT_EXAMPLES", prefix),
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: false,
+            permLevel: "Server Moderator",
+            botPermissions: ["EMBED_LINKS"],
+            aliases: ["joke", "blagues", "jokes"],
+            nsfw: false,
+            adminOnly: false,
+            cooldown: 2000,
+        });
+    }
+
+    run(message, args) {
+        try {
+            if (message.settings.language !== "french") return message.channel.send(message.language.get("BLAGUE_NOT_AVALIABLE"));
+            fetch('https://www.blagues-api.fr/api/random', {
+                headers: {
+                    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTUzMTYzMzA4ODAxNzIwMzIxIiwibGltaXQiOjEwMCwia2V5IjoiYUl3YXoxSk5BeWVramtCS0d5VE1XbkRiTFRGOXk0bDFVOHJlTFlka0xCcXdyVzZhbVAiLCJjcmVhdGVkX2F0IjoiMjAyMC0wNC0xOVQyMzozMTozMyswMjowMCIsImlhdCI6MTU4NzMzMTg5M30.VtRtZDm1MuU23OCeJog8rCz1npFSUKqQ3MpbyHLaPII`
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    return message.channel.send({
+                        embed: {
+                            author: {
+                                name: message.bot.user.username,
+						        icon_url: message.bot.user.displayAvatarURL
+                            },
+                            fields: [
+                                {
+                                    name: message.language.get("BLAGUE_QUESTION"),
+                                    value: data.joke,
+                                },
+                                {
+                                    name: message.language.get("BLAGUE_ANSWER"),
+                                    value: data.answer,
+                                },
+                            ],
+                            color: message.config.embed.color,
+                            timestamp: new Date(),
+					        footer: {
+						        text: message.language.get("BLAGUE_FOOTER", data.type, data.id),
+					        },
+                        },
+                    });
+                })
+        } catch (error) {
+            console.error(error);
+            return message.channel.send(message.language.get("ERROR", error));
+        }
+    }
+}
+
+module.exports = Number;
