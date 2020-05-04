a4392640a548bcf6cdaae1a5b10515d786574833
LePtitMetalleux <iias.bv@gmail.com>
2020-05-05 00:38:23 +0200

    Update invalidated.js

--- events/invalidated.js
+++ events/invalidated.js
@@ -4,5 +4,7 @@ module.exports = class {
 	}
 
 	async run() {
+		await this.client.guilds.cache.get("697368051405815860").channels.cache.get("697379467689066558").send("<@!153163308801720321> <@!169146903462805504> | Event invalidated emitted, bot shutdowned, please check logs and restart bot");
+		this.client.destroy();
 	}
 };
