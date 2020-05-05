fae5cf5c236b1352114828ac67622eefb97df6a9
LePtitMetalleux <iias.bv@gmail.com>
2020-05-05 12:10:13 +0200

    Update guildUpdate

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -3,6 +3,9 @@ module.exports = class {
 		this.client = client;
 	}
 
-	async run() {
+	async run(oldGuild, newGuild) {
+		if (oldGuild.id !== newGuild.id || oldGuild.name !== newGuild.name || oldGuild.owner.user.tag !== newGuild.owner.user.tag){
+			return mysqlcon.query("UPDATE Guilds SET guild_id = ?, guild_name = ?, guild_owner = ? WHERE guild_id = ?", [newGuild.id, newGuild.name, newGuild.owner.user.tag, oldGuild.id]);
+		}
 	}
 };
