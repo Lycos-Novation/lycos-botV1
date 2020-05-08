4657d9428551370f6519f3423d1335f49c93b297
LePtitMetalleux <iias.bv@gmail.com>
2020-05-09 00:01:49 +0200

    Add verification role delete

--- events/roleDelete.js
+++ events/roleDelete.js
@@ -11,6 +11,17 @@ module.exports = class {
 		var g;
 		mysqlcon.query(sql, async function (err, result, fields) {
 			g = result[0];
+			var ar = result[0].autorole, ids = [];
+			if (ar.split("/").length > 1) {
+				for (var i = 0; i < ar.split("/").length; i++) {
+					if (ar.split("/")[i] !== rid) {
+						ids.push(ar.split("/")[i])
+					}
+				}
+				ids.join("/");
+				mysqlcon.query("UPDATE Guilds SET autorole = ? WHERE guild_id = ?", [ids, message.guild.id]);
+				return message.channel.send(message.language.get("AUTOROLE_ROLE_REMOVED", rid));
+			}
 		if(g.logs_channel === null) return;
 		const lang = new (require(`../languages/${g.language}.js`));
 		return role.guild.channels.cache.find(c => c.id === g.logs_channel).send({
